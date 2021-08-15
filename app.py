65cfc9624d6caacfeebc01b4467ad7beb6fdbf26
Sohrawordi <59916856+Sohrawordi@users.noreply.github.com>
2021-08-15 22:47:15 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -7,6 +7,64 @@ Created on Sun Aug 15 21:35:46 2021
 from flask import Flask, render_template
 
 
+def amino_acid_composition(dataset):
+    import pandas as pd
+    #A list of 21 Amino Acid
+    amino_acid="ACDEFGHIKLMNPQRSTWYVX"
+    colls=[]  
+    #sample = pd.read_csv(dataset)
+    
+    for i in range (len(amino_acid)):
+        colls.append("Amino_acid_"+amino_acid[i])
+    #print(colls)
+    df=pd.DataFrame(columns=colls)
+    for i in range(len(dataset['Sequence'])):
+        #print(i)
+        row={}
+        for acid in amino_acid:
+            #print(seq.count(acid))
+            row["Amino_acid_"+acid]=dataset['Sequence'][i].count(acid)/len(dataset['Sequence'])
+        df=df.append(row,ignore_index=True)
+
+    
+
+
+
+    return df
+
+
+def binary_data(positive_sample):
+    import pandas as pd
+    amino_acid="ACDEFGHIKLMNPQRSTWYVX"
+    binary_code={}
+    colls=[]    
+    for i in range (len(amino_acid)*len(positive_sample['Sequence'][0])):
+        colls.append("Binary"+str(i))
+
+    for i in amino_acid:
+        a=[]
+        for j in amino_acid:
+            if i==j:
+                a.append(1)
+            else:
+                a.append(0)  
+        binary_code[i]=a 
+        
+        
+        
+    pdf=pd.DataFrame(columns=colls)
+    for k in range(len(positive_sample['Sequence'])):
+        c=0 
+        row_value={}
+        for i in positive_sample['Sequence'][k]:
+            for j in binary_code[i]:
+                row_value["Binary"+str(c)]=j
+                c=c+1
+        ##row_value["Class"]=1        
+        pdf=pdf.append(row_value,ignore_index=True) 
+    
+    return pdf
+
 
 app = Flask(__name__)
 
@@ -24,4 +82,4 @@ def home():
 
 
 if __name__ == "__main__": 
-    app.run() 
+    app.run(debug=True) 
